19d92bc2eedc9b8d0816dba7811da3ace24bf629
arpitsingh17 <arpitsingh.17@gmail.com>
2016-07-01 20:42:41 -0700

    Update shopping_list.py

--- shopping_list.py
+++ shopping_list.py
@@ -8,6 +8,14 @@ while True:
 # check for done
     if (new_item == "DONE"):
         break
+    elif (new_item=="SHOW"):
+        for new_item in shopping_list:
+            print(new_item)
+    elif (new_item=="HELP"):
+        print("TYPE DONE to exit\n"
+              "Type SHOW to see the current items\n"
+              "Type DONE to exit adding and see the items\n")
+        continue
 #add new item to the list
     shopping_list.append(new_item)
 
