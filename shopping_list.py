decc628290297c8ea5767ba5e7a1dcdff9a7695d
arpitsingh17 <arpitsingh.17@gmail.com>
2016-07-01 21:09:54 -0700

    Update shopping_list.py

--- shopping_list.py
+++ shopping_list.py
@@ -1,24 +1,30 @@
 #list to hold new items
 shopping_list = []
 #message prompt for the user
+
+
+def show_help():
+    print("TYPE DONE to exit\n"
+          "Type SHOW to see the current items\n"
+          "Type DONE to exit adding and see the items\n")
+def print_list():
+    print("\n\nHere's your list")
+    for new_item in shopping_list:
+        print(new_item)
+    print("you have {} items".format(len(shopping_list)))
 print("Enter new item to add")
-print("Enter DONE to finish")
+show_help()
 while True:
     new_item = input("> ")
 # check for done
     if (new_item == "DONE"):
         break
     elif (new_item=="SHOW"):
-        for new_item in shopping_list:
-            print(new_item)
+        print_list()
     elif (new_item=="HELP"):
-        print("TYPE DONE to exit\n"
-              "Type SHOW to see the current items\n"
-              "Type DONE to exit adding and see the items\n")
+        show_help()
         continue
 #add new item to the list
     shopping_list.append(new_item)
 
-print("\n\nHere's your list")
-for new_item in shopping_list:
-    print(new_item)
+print_list()
