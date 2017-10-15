43b24f9b4446741faa7beab5a099e51a24e80705
Arpit Singh <arpitsingh@Arpits-MacBook-Pro.local>
2017-10-15 16:59:28 -0700

    Added new method for array rotation

--- array_rotation.py
+++ array_rotation.py
@@ -1,9 +1,22 @@
 # arr = [1,2,3,4,5] after rotating by 2 [3,4,5,1,2]
+
 def rotate(arr,d,n):
     temp = []
     temp = arr[:d]
     arr[:n-d] = arr[d:n]
     arr[-d:] = temp
-    print arr
-    print temp
+    print "Result by method 1",arr
+
 rotate([1,2,3,4,5],2,5)
+
+#################### Method 2 rotating one by one ###################
+def rotate_new(arr,d,n):
+    temp = arr[0]
+    for i in range(d):
+        temp = arr[0]
+        for i in range(len(arr)-1):
+
+            arr[i] = arr[i+1]
+        arr[len(arr)-1] = temp
+    print "result by method 2",arr
+rotate_new([1,2,3,4,5],2,5)
